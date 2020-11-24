593ffcdba416c6e1a13ab9c7982fdc38e83599f6
Dmitry <d.av89@yandex.ru>
2020-11-24 22:03:21 +0300

    create: add swiper slider in hotel section

---
+++ tour-plan/js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  spaceBetween: 90
+});
