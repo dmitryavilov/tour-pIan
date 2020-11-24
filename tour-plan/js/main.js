0782e2e1458fa2e754343cc720e69e8ace8dc70c
Dmitry <d.av89@yandex.ru>
2020-11-24 22:06:42 +0300

    small fix

--- tour-plan/js/main.js
+++ tour-plan/js/main.js
@@ -5,5 +5,9 @@ var mySwiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
   spaceBetween: 90
 });
