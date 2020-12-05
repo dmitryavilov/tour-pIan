2727794936c7886ec5fa2decf03a0ea34ec5bc42
Dmitry <d.av89@yandex.ru>
2020-12-05 20:06:10 +0300

    create: modal

--- tour-plan/js/main.js
+++ tour-plan/js/main.js
@@ -24,7 +24,35 @@ var reviewsSlider = new Swiper(".reviews-slider", {
   spaceBetween: 90,
 });
 
-const menuButton = document.querySelector('.menu-button');
+const menuButton = document.querySelector('.menu-button'),
+      modalOverlay = document.querySelector('.modal__overlay'),
+      modalDialog = document.querySelector('.modal__dialog'),
+      closeBtn = document.querySelector('.modal__close'),
+      modalButton = document.querySelector('[data-toggle=modal]');
+const openModal = () => {
+  modalOverlay.classList.add('modal__overlay--active');
+  modalDialog.classList.add('modal__dialog--active');
+  document.querySelector('body').style.overflowY="hidden";
+}
+
+const closeModal = (e) => {
+  event.preventDefault();
+  modalOverlay.classList.remove('modal__overlay--active');
+  modalDialog.classList.remove('modal__dialog--active');
+  document.querySelector('body').style.overflowY="scroll";
+}
+
+const keyPress = (e) => {
+  if (e.keyCode == 27){
+    modalOverlay.classList.remove('modal__overlay--active');
+    modalDialog.classList.remove('modal__dialog--active');
+    document.querySelector('body').style.overflowY="scroll";
+  }
+}
+modalButton.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+document.addEventListener('keydown', keyPress);
+
 menuButton.addEventListener('click', function(){
   document.querySelector('.navbar-menu').classList.toggle('navbar-menu_visible');
 });
