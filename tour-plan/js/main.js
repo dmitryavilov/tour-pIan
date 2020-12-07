7db5a84d5e3a2779b6ba9d32fb6957079a5dfee6
Dmitry <d.av89@yandex.ru>
2020-12-07 22:56:50 +0300

    create: validate

--- tour-plan/js/main.js
+++ tour-plan/js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper(".hotel-slider", {
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
   loop: true,
   navigation: {
     nextEl: ".hotel-slider__button--next",
@@ -56,5 +57,27 @@ document.addEventListener('keydown', keyPress);
 menuButton.addEventListener('click', function(){
   document.querySelector('.navbar-menu').classList.toggle('navbar-menu_visible');
 });
-
+$('.form').each(function(){
+  $(this).validate({
+  errorClass: "invalid",
+  messages: {
+    name: {
+      required: "Please specify your name",
+      minlength: "Min count of symbols - 2"
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+    phone:{
+      required: 'Please specify your phone number'
+    }
+  }
+  });
+});
+$('.input__phone').each(function(){
+  $(this).mask('+7(000)000-00-00');
+});
 $(".newsletter").parallax({ imageSrc: "./img/newsletter/bg.jpg" });
+
+});
